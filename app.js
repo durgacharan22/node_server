b4a56b77255568b6f799f04f15be8e22c2b8cefa
durgacharan22 <durgacharanpotukuru@gmail.com>
2019-04-01 10:24:53 -0400

    added nodegjvhkb

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const users = require('./routes/users');
 const posts = require('./routes/posts');
 const comments = require('./routes/comments');
-const port = 'https://nodeserver22.herokuapp.com/';
+const port = '3000';
 require('./db/db')();
 // app.use(db);
 app.use(cors());
